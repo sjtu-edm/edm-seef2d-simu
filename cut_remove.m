e6d02cb3b00c774935f53b91e1705ca3aec21459
jypei <jypei@sjtu.edu.cn>
2015-04-03 16:56:54 -0400

    fix typo

--- cut_remove.m
+++ cut_remove.m
@@ -9,7 +9,7 @@ end
 
 for i = max((cur_t_x - ele_range_t), 1):min((cur_t_x + ele_range_t), tool_h)
     ele_range_t_y = round(sqrt(ele_range_t^2 - (i - cur_t_x)^2));
-    for j = max((cur_t_y - ele_range_t_y), 1):min((cur_t_y + ele_range_t_y), tool_w);
+    for j = max((cur_t_y - ele_range_t_y), 1):min((cur_t_y + ele_range_t_y), tool_w)
         tool(i, j) = 1;
     end
 end
@@ -21,7 +21,7 @@ cur_d_y = min_drill_x;
 
 for i = max((cur_d_x - ele_range_d), 1):min((cur_d_x + ele_range_d), drill_h)
     ele_range_d_y = round(sqrt(ele_range_d^2 - (i - cur_d_x)^2));
-    for j = max((cur_d_y - ele_range_d_y), 1):min((cur_d_y + ele_range_d_y), drill_l);
+    for j = max((cur_d_y - ele_range_d_y), 1):min((cur_d_y + ele_range_d_y), drill_l)
         drill(i, j) = 1;
     end
 end
